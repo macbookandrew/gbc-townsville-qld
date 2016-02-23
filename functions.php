09a567b1ce177a0da11e2a56507cbdf3083219f2
Andrew Minion <andrew@andrewrminion.com>
2016-02-23 15:43:54 -0500

    add devotionals post type

--- functions.php
+++ functions.php
@@ -37,3 +37,66 @@ function add_doctrinal_statement_JS() {
     }
 }
 add_action( 'wp_enqueue_scripts', 'add_doctrinal_statement_JS' );
+
+// add devotionals post type
+// Register Custom Post Type
+function daily_devotional() {
+
+	$labels = array(
+		'name'                  => 'Devotionals',
+		'singular_name'         => 'Devotional',
+		'menu_name'             => 'Devotional',
+		'name_admin_bar'        => 'Devotional',
+		'archives'              => 'Devotional Archives',
+		'parent_item_colon'     => 'Parent Devotional:',
+		'all_items'             => 'All Devotionals',
+		'add_new_item'          => 'Add New Devotional',
+		'add_new'               => 'Add New',
+		'new_item'              => 'New Devotional',
+		'edit_item'             => 'Edit Devotional',
+		'update_item'           => 'Update Devotional',
+		'view_item'             => 'View Devotional',
+		'search_items'          => 'Search Devotional',
+		'not_found'             => 'Not found',
+		'not_found_in_trash'    => 'Not found in Trash',
+		'featured_image'        => 'Featured Image',
+		'set_featured_image'    => 'Set featured image',
+		'remove_featured_image' => 'Remove featured image',
+		'use_featured_image'    => 'Use as featured image',
+		'insert_into_item'      => 'Insert into item',
+		'uploaded_to_this_item' => 'Uploaded to this devotional',
+		'items_list'            => 'Devotionals list',
+		'items_list_navigation' => 'Devotionals list navigation',
+		'filter_items_list'     => 'Filter devotionals list',
+	);
+	$rewrite = array(
+		'slug'                  => 'devotional',
+		'with_front'            => true,
+		'pages'                 => true,
+		'feeds'                 => true,
+	);
+	$args = array(
+		'label'                 => 'Devotionals',
+		'description'           => 'Devotional',
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'excerpt', 'author', 'comments', ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-book-alt',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'rewrite'               => $rewrite,
+		'capability_type'       => 'post',
+	);
+	register_post_type( 'devotional', $args );
+
+}
+add_action( 'init', 'daily_devotional', 0 );
